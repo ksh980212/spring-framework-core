6c254d0ca40a703cce1b3739e03b3e4d1eb3e9b7
ksh980212 <ksh980212@gmail.com>
2020-02-25 19:21:53 +0900

    Initial commit

---
+++ src/main/java/com/springframework/free/demo/AppRunner.java
@@ -0,0 +1,15 @@
+package com.springframework.free.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+@RequiredArgsConstructor
+public class AppRunner implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
+
+}
