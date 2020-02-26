3312378a8622c594ee71f227884025a57ba1f48e
ksh980212 <ksh980212@gmail.com>
2020-02-26 19:32:55 +0900

    14. Validation 추상화

--- src/main/java/com/springframework/free/demo/AppRunner.java
+++ src/main/java/com/springframework/free/demo/AppRunner.java
@@ -3,13 +3,30 @@ package com.springframework.free.demo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
+@Component
 public class AppRunner implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		Event event = new Event();
+		EventValidator eventValidator = new EventValidator();
+		Errors errors = new BeanPropertyBindingResult(event, "event");
 
+		eventValidator.validate(event, errors);
+
+		System.out.println(errors.hasErrors());
+		errors.getAllErrors().forEach(error -> {
+			System.out.println("====== error code ====");
+			Arrays.stream(error.getCodes()).forEach(System.out::println);
+			System.out.println(error.getDefaultMessage());
+		});
 	}
 
 }
---
+++ src/main/java/com/springframework/free/demo/Event.java
@@ -0,0 +1,13 @@
+package com.springframework.free.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Event {
+
+	Long id;
+	String title;
+
+}
